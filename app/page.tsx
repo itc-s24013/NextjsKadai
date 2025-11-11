de8971c12bb2fa32b3a81fc0f550cce070526c40
itc-s24013 <s24013@std.it-college.ac.jp>
2025-11-11 10:16:14 +0900

    トップページを作成

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,19 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <Image
+        src="/image.png"
+        alt="こもれびロゴ"
+        width={1920}
+        height={576}
+        className={styles.bgImage}
+      />
+      <section className={styles.news}>
+        <h1 className={styles.title}>こもれび</h1>
+        <p className={styles.description}>あなただけの隠れ家</p>
+      </section>
+    </main>
+  );
 }
