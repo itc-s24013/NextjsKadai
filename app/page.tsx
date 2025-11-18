190b07dfd81052517117a0e22df239d377db9cb6
itc-s24013 <s24013@std.it-college.ac.jp>
2025-11-18 10:02:14 +0900

    情報が入っていない時のデフォルトを設定

--- app/page.tsx
+++ app/page.tsx
@@ -11,22 +11,49 @@ export default async function Page() {
         <p className={styles.description}>あなただけの隠れ家</p>
         <Image
           src="/image.png"
-          alt="こもれびロゴ"
+          alt=""
           width={1920}
           height={576}
           className={styles.bgimg}
         />
       </section>
       <section className={styles.news}>
-        {data.contents.map((item) => (
-          <Image
-            key={item.id}
-            src={item.image?.url}
-            alt=""
-            width={item.image?.width}
-            height={item.image?.height}
-          />
-        ))}
+        <h2 className={styles.newsTitle}>Menu</h2>
+        <ul>
+          {data.contents.map((item) => (
+            <li key={item.id} className={styles.list}>
+              {item.image ? (
+                <div className={styles.link}>
+                  <Image
+                    src={item.image?.url}
+                    alt=""
+                    width={256}
+                    height={256}
+                  />
+                </div>
+              ) : (
+                <Image src="/no-img.png" alt="" width={256} height={256} />
+              )}
+              <dl className={styles.content}>
+                <dt className={styles.name}>{item.name}</dt>
+                <dt className={styles.meta}>
+                  <span className={styles.tag}>
+                    {item.taste && item.taste.length > 0 ? (
+                      item.taste.map((taste) => (
+                        <p key={taste.id}>{taste.flavor}</p>
+                      ))
+                    ) : (
+                      <p>味の情報はありません</p>
+                    )}
+                  </span>
+                  <span className={styles.tag}>
+                    <p>{item.amount}円</p>
+                  </span>
+                </dt>
+              </dl>
+            </li>
+          ))}
+        </ul>
       </section>
     </main>
   );
