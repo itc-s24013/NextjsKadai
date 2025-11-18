0de1a7129cc76c55dadf024bc11904a6cd9393c9
itc-s24013 <s24013@std.it-college.ac.jp>
2025-11-18 11:39:01 +0900

    menuをcardで表示する

--- app/page.tsx
+++ app/page.tsx
@@ -18,40 +18,59 @@ export default async function Page() {
         />
       </section>
       <section className={styles.news}>
-        <h2 className={styles.newsTitle}>Menu</h2>
-        <ul>
+        <h2 className="text-center">Menu</h2>
+        <br />
+        <ul className="row">
           {data.contents.map((item) => (
-            <li key={item.id} className={styles.list}>
-              {item.image ? (
-                <div className={styles.link}>
-                  <Image
-                    src={item.image?.url}
-                    alt=""
-                    width={256}
-                    height={256}
-                  />
+            <>
+              <li key={item.id} className="col-5">
+                <div className="card">
+                  {item.image ? (
+                    <div className={styles.link}>
+                      <Image
+                        src={item.image?.url}
+                        className="card-img-top"
+                        alt=""
+                        width={256}
+                        height={256}
+                      />
+                    </div>
+                  ) : (
+                    <Image
+                      src="/no-img.png"
+                      className="card-img-top"
+                      alt=""
+                      width={256}
+                      height={256}
+                    />
+                  )}
+                  <dl className="card-body">
+                    <dt className="card-title">{item.name}</dt>
+                    <dt className={styles.meta}>
+                      <span className={styles.tag}>
+                        {item.taste && item.taste.length > 0 ? (
+                          item.taste.map((taste) => (
+                            <p key={taste.id} className="card-text">
+                              {taste.flavor}
+                            </p>
+                          ))
+                        ) : (
+                          <p className="card-text">味の情報はありません</p>
+                        )}
+                      </span>
+                      <span className={styles.tag}>
+                        <p className="card-text">{item.amount}円</p>
+                      </span>
+                    </dt>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </dl>
                 </div>
-              ) : (
-                <Image src="/no-img.png" alt="" width={256} height={256} />
-              )}
-              <dl className={styles.content}>
-                <dt className={styles.name}>{item.name}</dt>
-                <dt className={styles.meta}>
-                  <span className={styles.tag}>
-                    {item.taste && item.taste.length > 0 ? (
-                      item.taste.map((taste) => (
-                        <p key={taste.id}>{taste.flavor}</p>
-                      ))
-                    ) : (
-                      <p>味の情報はありません</p>
-                    )}
-                  </span>
-                  <span className={styles.tag}>
-                    <p>{item.amount}円</p>
-                  </span>
-                </dt>
-              </dl>
-            </li>
+                <br />
+              </li>
+              <li className="col-1"></li>
+            </>
           ))}
         </ul>
       </section>
