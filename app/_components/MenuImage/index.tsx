3f4aa895142b67729dac0319d3c333e95762f30a
itc-s24013 <s24013@std.it-college.ac.jp>
2025-11-19 12:40:34 +0900

    画像を表示するコンポーネント

---
+++ app/_components/MenuImage/index.tsx
@@ -0,0 +1,15 @@
+import { MicroCMSImage } from "microcms-js-sdk";
+import styles from "./index.module.css";
+import Image from "next/image";
+
+export default function MenuImage({ img }: { img?: MicroCMSImage }) {
+  return (
+    <div className={styles.img}>
+      {img ? (
+        <Image src={img.url} alt="" fill={true} />
+      ) : (
+        <Image src="/no-img.png" alt="" fill={true} />
+      )}
+    </div>
+  );
+}
