5c3906f8b07b41a81fd7bf28af6ad886b43c86e6
itc-s24013 <s24013@std.it-college.ac.jp>
2025-11-19 10:52:47 +0900

    店名とキャッチコピー、指定した部分の背景を白くするのを消した

--- app/page.tsx
+++ app/page.tsx
@@ -6,67 +6,64 @@ export default async function Page() {
   const data = await getMenuList();
   return (
     <div>
-      <section className={styles.top}>
-        <h1 className={styles.title}>こもれび</h1>
-        <h2 className={styles.description}>〜 あなただけの隠れ家 〜</h2>
-      </section>
-      <section className={styles.news}>
-        <h2 className="text-center">〜 Menu 〜</h2>
-        <br />
-        <ul className="row">
-          {data.contents.map((item) => (
-            <>
-              <li key={item.id} className="col-3">
-                <div className="card">
-                  {item.image ? (
-                    <div className={styles.link}>
-                      <Image
-                        src={item.image?.url}
-                        className="card-img-top"
-                        alt=""
-                        width={256}
-                        height={256}
-                      />
-                    </div>
-                  ) : (
+      <h2>〜 Menu 〜</h2>
+      <br />
+      <ul className="row">
+        {data.contents.map((item) => (
+          <>
+            <li key={item.id} className="col-3">
+              <div className="card">
+                {item.image ? (
+                  <div className={styles.link}>
                     <Image
-                      src="/no-img.png"
+                      src={item.image?.url}
                       className="card-img-top"
                       alt=""
                       width={256}
                       height={256}
                     />
-                  )}
-                  <dl className="card-body">
-                    <dt className="card-title">{item.name}</dt>
-                    <dt className={styles.meta}>
-                      <span className={styles.tag}>
-                        {item.taste && item.taste.length > 0 ? (
-                          item.taste.map((taste) => (
-                            <p key={taste.id} className="card-text">
-                              {taste.flavor}
-                            </p>
-                          ))
-                        ) : (
-                          <p className="card-text">味の情報はありません</p>
-                        )}
-                      </span>
-                      <span className={styles.tag}>
-                        <p className="card-text">{item.amount}円</p>
-                      </span>
-                    </dt>
-                    <br />
-                    <a href="#" className="btn btn-outline-secondary">
-                      Go somewhere
-                    </a>
-                  </dl>
-                </div>
-                <br />
-              </li>
-            </>
-          ))}
-        </ul>
-      </section>
+                  </div>
+                ) : (
+                  <Image
+                    src="/no-img.png"
+                    className="card-img-top"
+                    alt=""
+                    width={256}
+                    height={256}
+                  />
+                )}
+                <dl className="card-body">
+                  <dt className="card-title">{item.name}</dt>
+                  <dt className={styles.meta}>
+                    <span className={styles.tag}>
+                      {item.taste && item.taste.length > 0 ? (
+                        item.taste.map((taste) => (
+                          <p key={taste.id} className="card-text">
+                            {taste.flavor}
+                          </p>
+                        ))
+                      ) : (
+                        <p className="card-text">味の情報はありません</p>
+                      )}
+                    </span>
+                    <span className={styles.tag}>
+                      <p className="card-text">{item.amount}円</p>
+                    </span>
+                  </dt>
+                  <br />
+                  <a
+                    href={`./${item.id}`}
+                    className="btn btn-outline-secondary"
+                  >
+                    Go somewhere
+                  </a>
+                </dl>
+              </div>
+              <br />
+            </li>
+          </>
+        ))}
+      </ul>
     </div>
   );
 }
