3ff0353409f922f5df0fa9d09a4e9881642a6bec
itc-s24013 <s24013@std.it-college.ac.jp>
2025-11-19 12:41:25 +0900

    menuの情報を表示する

--- app/[menu]/page.tsx
+++ app/[menu]/page.tsx
@@ -1,4 +1,6 @@
 import { getMenuDetail } from "../_libs/microcms";
+import styles from "../page.module.css";
+import MenuImage from "../_components/MenuImage";
 
 type Props = {
   params: {
@@ -9,5 +11,26 @@ type Props = {
 export default async function Page(props: Props) {
   const data = await getMenuDetail(props.params.menu);
 
-  return <div>{data.name}</div>;
+  return (
+    <>
+      <br />
+      <MenuImage img={data.image} />
+      <dl>
+        <dt>{data.name}</dt>
+        <dt className={styles.meta}>
+          <span className={styles.tag}>
+            {data.taste && data.taste.length > 0 ? (
+              data.taste.map((taste) => <p key={taste.id}>{taste.flavor}</p>)
+            ) : (
+              <p>味の情報はありません</p>
+            )}
+          </span>
+          <span className={styles.tag}>
+            <p>{data.amount}円</p>
+          </span>
+        </dt>
+        <br />
+      </dl>
+    </>
+  );
 }
