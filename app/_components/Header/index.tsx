024fe9349d36de6dabeca6170a7603406115601d
itc-s24013 <s24013@std.it-college.ac.jp>
2025-11-20 14:16:21 +0900

    ヘッダーをホーム画面へのリンクにした

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -3,7 +3,9 @@ import styles from "./index.module.css";
 export default function Header() {
   return (
     <section className={styles.top}>
-      <h1 className={styles.title}>こもれび</h1>
+      <h1 className={styles.title}>
+        <a href="/">こもれび</a>
+      </h1>
       <h2 className={styles.description}>〜 あなただけの隠れ家 〜</h2>
     </section>
   );
